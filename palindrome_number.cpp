3adc82261ffa8585e23d363402897b510eb44f1b
yangwei <xdgodyw@gmail.com>
2018-07-31 19:44:18 +0800

    complete two more problems

--- palindrome_number.cpp
+++ palindrome_number.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<list>
+#include<vector>
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
@@ -35,9 +37,118 @@ public:
         }
         return str;
     }
+    //罗马字母转换成数字
+    //思路：使用while循环遍历字符串，当发现第一个是有I或X或C的时候查看第二个数是否
+    //为V、X或L、C或D、M，如果是的话就第二个数减去第一个数，然后迭代器+2,不然直接相加，迭代器+1
+    int romanToInt(string s) {
+    auto ss=s.begin();
+    int sum=0;
+    while(ss!=s.end()){
+        if(*ss=='I'){
+            switch (*(ss+1))
+            {
+                case 'V':
+                    sum+=4;
+                    ss=ss+2;
+                    break;
+                case 'X':
+                    sum+=9;
+                    ss=ss+2; 
+                    break;           
+                default:
+                    sum+=1;
+                    ss=ss+1;
+                    break;
+            }
+        }else if(*ss=='X'){
+            switch (*(ss+1))
+            {
+                case 'L':
+                    sum+=40;
+                    ss=ss+2;
+                    break;
+                case 'C':
+                    sum+=90;
+                    ss=ss+2;
+                    break;            
+                default:
+                    sum+=10;
+                    ss=ss+1;
+                    break;
+            }
+        }else if(*ss=='C'){
+            switch (*(ss+1))
+            {
+                case 'D':
+                    sum+=400;
+                    ss=ss+2;
+                    break;
+                case 'M':
+                    sum+=900;
+                    ss=ss+2;  
+                    break;          
+                default:
+                    sum+=100;
+                    ss=ss+1;
+                    break;
+            }
+        }else if(*ss=='V'){
+            sum+=5;
+            ss++;
+        }else if(*ss=='L'){
+            sum+=50;
+            ss++;
+        }else if(*ss=='D'){
+            sum+=500;
+            ss++;
+        }else if(*ss=='M'){
+            sum+=1000;
+            ss++;
+        }
+    }   
+    return sum; 
+    }
+    /*
+    最长公共前缀
+    思路：首先使用stl中的sort方法将vector中的字符串按照长度大小从小到大排列
+    将第一个字符串也就是最短的字符串当作比较的对象，一个while循环遍历这个字符串
+    另一个for循环遍历vector对象，如果发现有相同下标的值不相同即break退出
+    否则就继续遍历
+    */
+    string longestCommonPrefix(vector<string>& strs) {
+    sort(strs.begin(),strs.end(),[](string &a, string &b){  
+    return a.size() < b.size();  
+});
+    string s1=strs[0];
+    int i=0;
+    int j=0;
+    while(i!=s1.size()){
+        bool flag=1;
+        for(j=0;j!=strs.size();j++){
+            if(strs[j][i]!=s1[i]){
+                flag=0;
+                break;
+            }
+            if(flag==0){
+            break;}
+        }
+       if(flag==0){
+            break;}
+            else{
+                i++;
+            }
+    }
+    //cout<<i<<endl;
+    string ss="";
+    for(int k=0;k<i;k++){
+        ss+=s1[k];
+    }
+    return ss;
+    }
 };
 int main(){
     Solution s;
-    cout<<s.toLowerCase("ABCdEf");
+    vector<string> v1={"dog","docecar","dor"};
+    cout<<s.longestCommonPrefix(v1);
     return 0;
 }
