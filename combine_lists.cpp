5df28ee1f59b895ead416744038b92c3359f0de3
yangwei <xdgodyw@gmail.com>
2018-08-02 23:17:44 +0800

    two more problems

--- combine_lists.cpp
+++ combine_lists.cpp
@@ -72,6 +72,46 @@ public:
         }
         return nums.size();
     }
+    //数组中移除给定的元素值
+    //思路：排序完后使用find找到这个值的第一个位置，count找到有多少个，然后全部erase掉
+    int removeElement(vector<int>& nums, int val) {
+        sort(nums.begin(),nums.end());
+    auto x=find(nums.begin(),nums.end(),val);
+    auto y=count(x,nums.end(),val);
+    nums.erase(x,x+y);
+    return nums.size();
+    }
+    //返回一个字符串在另一个字符串中第一次出现的位置
+    //思路：当needle为空字符时，返回0，else直接使用STL中的find方法
+    int strStr(string haystack, string needle) {
+        if(needle==""){
+            return 0;
+        }else{
+        auto x=haystack.find(needle);
+        if(x==string::npos){
+            return -1;
+        }else{
+            return x;
+        }
+        }
+    }
+    //查询有序数组中的一个数值，若存在返回其索引，若不存在将其按顺序插入并返回索引
+    //思路：find方法查询数值，若迭代器不为end，则用当前迭代器减去begin即为下标
+    //若迭代器为end，则插入，使用for循环方法找到第一个比target大的值，然后插入起前面
+    int searchInsert(vector<int>& nums, int target) {
+        auto x=find(nums.begin(),nums.end(),target);
+        if(x!=nums.end()){
+            return x-nums.begin();
+        }else{
+            auto y=0;
+            for(y=0;y<nums.size();y++){
+                if(nums[y]>target)
+                break;
+            }
+            nums.insert(nums.begin()+y,target);
+            return y;
+        }
+    }
 };
 int main(){
     ListNode *l1=new ListNode(1);ListNode *l2=new ListNode(2);ListNode *l3=new ListNode(3);ListNode *l4=new ListNode(1);ListNode *l5=new ListNode(5);ListNode *l6=new ListNode(6);
