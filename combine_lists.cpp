d2c864d3050aec703c6f7cc85d31e8acffd90c97
yangwei <xdgodyw@gmail.com>
2018-08-02 21:59:40 +0800

    today's problem

--- combine_lists.cpp
+++ combine_lists.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 struct ListNode {
       int val;
@@ -30,4 +32,34 @@ public:
             }
         }
     }
+//去除一个数组中的重复的数值
+//思路一：使用std中的unique函数，然后再把重复的部分erase掉
+    int removeDuplicates(vector<int>& nums) {
+        //sort(nums.begin(),nums.end());
+        nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        return nums.size();
+    }
+    //思路二：使用sort排序后，遍历整个数组，使用find和count函数找到第一次和此数出现的此数，把
+    //第一次出现的迭代器加1，再把两者之间的数都erase掉即可
+    int removeDuplicates2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        auto i=nums.begin();
+        //cout<<*i<<endl;
+        while(i!=nums.end()){
+            auto x=find(nums.begin(),nums.end(),*i);
+            cout<<*x<<endl;
+            auto y=count(nums.begin(),nums.end(),*i);
+            cout<<y<<endl;
+            nums.erase(x,x+y-1);
+            
+            i++;
+        }
+        return nums.size();
+    }
 };
+int main(){
+    Solution s;
+    vector<int> v1={1,1,2};
+    cout<<s.removeDuplicates2(v1)<<endl;
+    return 0;
+}
