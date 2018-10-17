1c2c443391624b19cac754b7881ca92b89104d69
yangwei <632624562@qq.com>
2018-10-17 22:53:00 +0800

    xunlei_2018_xiaozhao 2 peogramming questions

--- test_function.cpp
+++ test_function.cpp
@@ -9,14 +9,11 @@ bool compare(int a,int b){
     return a>b?true:false;
 }
 int main(){
-   vector<int> v1={1,2,2};
-   v1.insert(v1.end(),2,2);
-   while(next_permutation(v1.begin(),v1.end())){
-       for(auto x:v1){
-           cout<<x<<" ";
-       }
-       cout<<endl;
-   }
+   char *str = "Thunder Network";
+vector<int> v;
+auto x=v.begin();
+vector<int>::iterator xx=x;
+printf("%s\n", str);
    
     return 0;
 }
