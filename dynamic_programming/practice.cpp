327893ed2122ed7ce0109f724ff4f6e90a4d321f
xdyangwei <xdgodyw@gmail.com>
2018-12-27 23:15:30 +0800

    dp again

--- dynamic_programming/practice.cpp
+++ dynamic_programming/practice.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include<cmath>
 #include<set>
+#include<string>
+#include<algorithm>
 using namespace std;
 
 int count_of_steps(int n, vector<int> &p) {
@@ -86,9 +88,9 @@ void longest_common_squence(vector<char> v1,vector<char> v2) {
 	cout << lcs(v, v1, v2, v1.size(), v2.size());
 }
 
-int max(int a,int b) {
-	return a > b ? a : b;
-}
+//int max(int a,int b) {
+//	return a > b ? a : b;
+//}
 int lcs(vector<vector<int>> &v,vector<char> v1,vector<char> v2,int i,int j) {
 	if (!i || !j)
 		return 0;
@@ -117,67 +119,140 @@ long combine_money(int n) {
 	
 }
 
-int lpd(string s, vector<vector<int>> &v, int i, int j);
 
 string longestPalindrome(string s) {
-	vector<vector<int>> v;
-	for (int i = 0; i < s.size(); i++) {
-		vector<int> v1;
-		for (int j = 0; j < s.size(); j++) {
-			if (i == j)
-				v1.push_back(1);
-			else if (j - i == 1) {
-				if (s[i] == s[j])
-					v1.push_back(1);
-				else
-				{
-					v1.push_back(0);
-				}
-			}
-			else
-			{
-				v1.push_back(0);
+	int max_len = 0,k,j,start;
+	for (int i = 0; i < s.size()-1;i++) {
+		while (s[i] == s[i + 1]) {
+			i++;
+		}
+		k = i + 1;
+		j = i;
+		while (s[j - 1] == s[k + 1] && j > 0 && k < s.size() - 1)
+		{
+			++k; --j;
+		}
+		if (k - j + 1 > max_len) {
+			max_len = k - j + 1;
+			start = j;
+		}
+	}
+	return s.substr(start, max_len);
+}
+
+int maxSubArray(vector<int>& nums) {
+	int res = nums[0], temp = 0;
+	for (int i = 0; i < nums.size(); i++) {
+		temp += nums[i];
+		if (temp > res) res = temp;
+		if (temp < 0) temp = 0;
+	}
+	return res;
+}
+
+int uniquePaths(int m, int n) {
+	vector<vector<int>> v(m, vector<int>(n, 0));
+	for (int i = 0; i < m; i++)
+		v[i][0] = 1;
+	for (int i = 0; i < n; i++)
+		v[0][i] = 1;
+	for (int i = 1; i < m; i++)
+		for (int j = 1; j < n; j++)
+			v[i][j] = v[i - 1][j] + v[i][j - 1];
+	return v[m - 1][n - 1];
+}
+
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+	auto m = obstacleGrid.size();
+	auto n = obstacleGrid[0].size();
+	vector<vector<int>> v(m, vector<int>(n, 0));
+	int m_flag = 0,n_flag=0;
+	for (int i = 0; i < m; i++) {
+		if (obstacleGrid[i][0] == 1)
+			m_flag = 1;
+		if(m_flag==0)
+		v[i][0] = 1;
+		else
+		{
+			v[i][0] = 0;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		if (obstacleGrid[0][i] == 1)
+			n_flag = 1;
+		if (n_flag)
+			v[0][i] = 0; else
+		{
+			v[0][i] = 1;
+		}
+		
+	}
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++) {
+			if (obstacleGrid[i][j] == 1) {
+				v[i][j] = 0;
 			}
 		}
-		v.push_back(v1);
-	}
-	lpd(s, v, 0, s.size()-1);
-	for (auto x : v)
-	{
-		for (auto xx : x)
-			cout << xx << " ";
-		cout << endl;
-	}
-	pair<int, int> p(0,0);
-	for (int i = 0; i < s.size(); i++) {
-		for(int j=i;j<s.size();j++)
-			if (v[i][j] == 1) {
-				if (j - i > p.second - p.first) {
-					p.first = i;
-					p.second = j;
+	for (int i = 1; i < m; i++)
+		for (int j = 1; j < n; j++) {
+			if (obstacleGrid[i][j] == 1) {
+				v[i][j] = 0;
+			}
+			else {
+				if (obstacleGrid[i - 1][j] == 1)
+					v[i][j] = v[i][j - 1];
+				else if (obstacleGrid[i][j - 1] == 1)
+					v[i][j] = v[i - 1][j];
+				else {
+					v[i][j] = v[i - 1][j] + v[i][j - 1];
 				}
 			}
-	}
-	string ss;
-	for (int i = p.first; i <= p.second; i++)
-		ss += s[i];
-	return ss;
+		}
+	return v[m - 1][n - 1];
 }
-int lpd(string s, vector<vector<int>> &v, int i, int j) {
-	if (i == j) {
-		v[i][j] = 1;
+
+int minPathSum(vector<vector<int>>& grid) {
+	auto m = grid.size();
+	auto n = grid[0].size();
+	vector<vector<int>> v(m, vector<int>(n, 0));
+	v[0][0] = grid[0][0];
+	for (int i = 1; i < m; i++) {
+		v[i][0] = v[i-1][0]+grid[i][0];
+	}
+	for (int i = 1; i < n; i++)
+		v[0][i] = grid[0][i]+v[0][i-1];
+	for(int i=1;i<m;i++)
+		for (int j = 1; j < n; j++) {
+			v[i][j] = min(v[i - 1][j], v[i][j - 1])+grid[i][j];
+		}
+	return v[m - 1][n - 1];
+}
+
+int numDecodings(string s) {
+	if (s.length() == 0) {
 		return 0;
 	}
-	else if (i < j) {
-		v[i][j] = 0;
-		return 0;
+	int n = s.length();
+	vector<int> dp(n + 1);
+	dp[0] = 1;
+	dp[1] = s[0] != '0' ? 1 : 0;
+	for (int i = 2; i <= n; i++) {
+		int first = s[i - 1] - '0';
+		int second = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+		if (first >= 1 && first <= 9) {
+			dp[i] += dp[i - 1];
+		}
+		if (second >= 10 && second <= 26) {
+			dp[i] += dp[i - 2];
+		}
 	}
-	else {
-		v[i][j] = (v[i + 1][j - 1]) && (s[i]==s[j]);
-		lpd(s, v, i + 1, j - 1);
-	}
+	return dp[n];
 }
+
+
 int main() {
-	cout<<longestPalindrome("caba");
+	vector<vector<int>> v(2, vector<int>({0,0}));
+	v[0] = { 0,1 }; v[1] = { 1,0 }; 
+	cout << numDecodings("230");
 	system("pause");
 }
