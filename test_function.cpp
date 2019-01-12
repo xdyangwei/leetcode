ead763612b5e94229428a4f7c234843908b75b23
xdyangwei <xdgodyw@gmail.com>
2019-01-12 23:20:00 +0800

    class test

--- test_function.cpp
+++ test_function.cpp
@@ -15,9 +15,60 @@ void pointer_plus(int *&(p))
     p++;
     cout << p << endl;
 }
+
+class animal
+{
+  protected:
+  public:
+    int width;
+    int height;
+    animal(int w, int h) : height(h), width(w)
+    {
+    }
+    void setwidth(int w)
+    {
+        width = w;
+    }
+    void setheight(int h)
+    {
+        height = h;
+    }
+    virtual int getheight()
+    {
+        return height;
+    }
+    virtual int getwidth()
+    {
+        return width;
+    }
+    virtual ~animal() = default;
+};
+
+class rabbit : public animal
+{
+  public:
+    rabbit(int h, int w, int n) : animal(w, h), name(n) {}
+    int name;
+    int getheight() override
+    {
+        return height * 2;
+    }
+    int getwidth() override
+    {
+        return width * 3;
+    }
+};
+
 int main()
 {
-    int i=0x12345678;
-    cout<<"good"<<endl;
+    rabbit a(1, 1, 1);
+    animal bb(1,1);
+    animal aa(2,2);
+    animal *b=&(bb);
+    *b = aa;
+    // b->height=5;
+    // b->width=5;
+    cout << b->height<< endl;
+    cout << b->width << endl;
     return 0;
 }
