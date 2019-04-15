816d1f7242c23197d96a29bf764f52da0330065d
xdyangwei <xdgodyw@gmail.com>
2019-04-15 16:39:09 +0800

    reconstruct the stack and queue with each other

---
+++ bytedance/queue_with_stack.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<cassert>
+#include "queue_with_stack.h"
+using namespace std;
+void queue_with_stack::push(int node) {
+	s1.push(node);
+}
+int queue_with_stack::pop() {
+	int x;
+	if (s2.empty()) {
+		if (s1.empty())
+			assert(!s1.empty());
+			while (!s1.empty()) {
+				auto x = s1.top();
+				s2.push(x);
+				s1.pop();
+			}
+			x = s2.top();
+			s2.pop();
+	}
+	else {
+		x = s2.top();
+		s2.pop();
+	}
+	return x;
+}
+queue_with_stack::queue_with_stack() = default;
+queue_with_stack::~queue_with_stack() = default;
+
+void stack_with_queue::push(int node) {
+	if (q2.empty())
+		q1.push(node);
+	else {
+		q2.push(node);
+	}
+}
+int stack_with_queue::pop() {
+	if (q1.empty() && q2.empty())
+		return -1;
+	if (q1.empty()) {
+		while (q2.size() != 1) {
+			auto x = q2.front();
+			q1.push(x);
+			q2.pop();
+		}
+		auto x = q2.front(); q2.pop();
+		return x;
+	}
+	else {
+		while (q1.size() != 1) {
+			auto x = q1.front();
+			q2.push(x);
+			q1.pop();
+		}
+		auto x = q1.front(); q1.pop();
+		return x;
+	}
+}
---
+++ bytedance/queue_with_stack.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<stack>
+#include<queue>
+class queue_with_stack
+{
+public:
+	void push(int);
+	int pop();
+	queue_with_stack();
+	~queue_with_stack();
+private:
+	std::stack<int> s1;
+	std::stack<int> s2;
+};
+class stack_with_queue
+{
+public:
+	void push(int);
+	int pop();
+	stack_with_queue()=default;
+	~stack_with_queue()=default;
+private:
+	std::queue<int> q1;
+	std::queue<int> q2;
+};
+
+
