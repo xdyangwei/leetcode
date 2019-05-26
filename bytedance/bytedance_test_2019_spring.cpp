b47fbb24913c69c79bf87ee6c26b3bd366df9bb0
xdyangwei <xdgodyw@gmail.com>
2019-05-26 14:05:20 +0800

    continue practicing backtracing

--- bytedance/bytedance_test_2019_spring.cpp
+++ bytedance/bytedance_test_2019_spring.cpp
@@ -97,11 +97,37 @@ void right_value_test(int&& a) {
 	cout << a << endl;
 	a = 1;
 }
+
+//地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，
+//每一次只能向左，右，上，下四个方向移动一格，但是不能进入行坐标和列坐标的数位之和大于k的格子。
+//使用回溯法
+static stack<pair<int,int>> s;
+int recursive_movingCount(vector<vector<int>>& flag,int m,int n,int rows,int cols,int& count) {
+	if (m < rows&&n < cols&&m >= 0 && n >= 0 && flag[m][n] == 0) {
+		flag[m][n] = 1;
+		s.push(make_pair(m,n));
+		count += recursive_movingCount(flag, m - 1, n, rows, cols, count);
+		count += recursive_movingCount(flag, m, n + 1, rows, cols, count);
+		count += recursive_movingCount(flag, m + 1, n, rows, cols, count);
+		count += recursive_movingCount(flag, m, n - 1, rows, cols, count);
+		return 1;
+	}
+	else {
+		if (s.size() == 1)
+			return count;
+		s.pop();
+		//auto x = s.top();
+		return 0;
+	}
+
+}
+int movingCount(int threshold, int rows, int cols) {
+	vector<vector<int>> v(rows,vector<int>(cols,0));
+	int count = 0;
+	return recursive_movingCount(v, 0, 0, rows, cols, count);
+}
+
 int main() {
-	char a[] = "123";
-	char x = 'a';
-	char* b = &x;
-	char** c = &b;
-	strtok_s(NULL,a,c);
+	cout << movingCount(10, 3, 4);
 	getchar();
 }
