fb3bc3c41a1e3ad8fec07295cb2af1b33e7951a0
xdyangwei <xdgodyw@gmail.com>
2019-08-05 17:56:56 +0800

    complete the calccapital problem

--- huawei_5.cpp
+++ huawei_5.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
+
+//字符串匹配，判断短字符串中的字符是否都在长字符串中
 void str_match(){
     string short_str,long_str;
     while(cin>>short_str>>long_str){
@@ -15,7 +18,57 @@ void str_match(){
             cout<<"false"<<endl;
     }
 }
+
+//真分数分解为埃及分数，埃及分数：分子为1的分数
+void pcRealFraction(){
+    char ch;
+    int a, b;
+    while (cin >> a >> ch >> b)
+    {
+        while (a != 1){
+            if (b % (a - 1) == 0){
+                cout << 1 << "/" << b / (a - 1) << "+";
+                a = 1;
+            }
+            else{
+                int c;
+                c = b / a + 1;
+                a = a - b%a;
+                b = b*c;
+                cout << 1 << "/" << c << "+";
+                if (b%a == 0){
+                    b = b / a;
+                    a = 1;
+                }
+            }
+        }
+        cout << 1 << "/" << b << endl;
+    }
+}
+
+//统计大写字母个数
+void CalcCapital(){
+    string str;
+    while(cin>>str){
+        int Count=0;
+        for(auto ss:str){
+            if(ss>='A'&&ss<='Z')
+            Count+=1;
+        }
+        cout<<Count<<endl;
+    }
+}
+
+//密码截取
+void max_passwd(){
+    string str;
+    while(cin>>str){
+        int Max=1;vector<int> v(str.size(),1);
+        
+    }
+}
+
 int main(){
-    str_match();
+    CalcCapital();
     return 0;
 }
