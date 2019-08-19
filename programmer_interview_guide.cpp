e47b25d0307d48cf1cfe191371743a67c6c52a12
xdyangwei <xdgodyw@gmail.com>
2019-08-19 22:35:53 +0800

    complete the stack_with_getmin problem

--- programmer_interview_guide.cpp
+++ programmer_interview_guide.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <stack>
+#include <string>
 using namespace std;
 void find_specific_number_in_matrix()
 {
@@ -131,7 +133,6 @@ void sum_equal_k()
                 j--;
             }
             j--;
-            
         }
         else
         {
@@ -142,8 +143,100 @@ void sum_equal_k()
     }
 }
 
+//给定排序数组arr和整数k，不重复打印arr中所有相加和为k的不降序三元组
+void sum_equal_k_2()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        int x = k - v[i];
+        while (v[i] == v[i + 1])
+            i++;
+        for (int j = i + 1, k = v.size() - 1; j < k;)
+        {
+            if (v[j] + v[k] == x)
+            {
+                cout << v[i] << " " << v[j] << " " << v[k] << endl;
+                while (v[j] == v[j + 1])
+                    j++;
+                j++;
+                while (v[k] == v[k - 1])
+                    k--;
+                k--;
+            }
+            else if (v[j] + v[k] > x)
+            {
+                while (v[k] == v[k - 1])
+                    k--;
+                k--;
+            }
+            else
+            {
+                while (v[j] == v[j + 1])
+                    j++;
+                j++;
+            }
+        }
+    }
+}
+
+void stack_with_getmin()
+{
+    stack<int> s1;
+    stack<int> s2;
+    int n;
+    cin >> n;
+    vector<string> v;
+    cin.get();
+    while (n)
+    {
+        string s;
+        getline(cin, s);
+        v.push_back(s);
+        n--;
+    }
+    for (auto vv : v)
+    {
+        auto it = vv.find(" ");
+        if (it != string::npos)
+        {
+            auto ss = stoi(vv.substr(it + 1));
+            s1.push(ss);
+            if (s2.empty())
+                s2.push(ss);
+            else
+            {
+                if (ss < s2.top())
+                    s2.push(ss);
+                else
+                    s2.push(s2.top());
+            }
+        }
+        else
+        {
+            if (vv == "pop")
+            {
+                s1.pop();
+                s2.pop();
+            }
+            if (vv == "getMin")
+            {
+                cout << s2.top() << endl;
+            }
+        }
+    }
+}
+
 int main()
 {
-    sum_equal_k();
+    stack_with_getmin();
     return 0;
 }
