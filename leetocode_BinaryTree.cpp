76bbbe630031925cacccfc8183104192f970998b
xdyangwei <xdgodyw@gmail.com>
2019-09-06 21:42:11 +0800

    complete the binary_tree flatten problem

--- leetocode_BinaryTree.cpp
+++ leetocode_BinaryTree.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include<cmath>
+#include<cstring>
+#include<string>
+#include<algorithm>
 using namespace std;
 struct TreeNode
 {
@@ -123,16 +127,64 @@ vector<int> preorderTraversal(TreeNode *root)
 //输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
 TreeNode *reConstructBinaryTree(vector<int> pre, vector<int> vin)
 {
-    if(pre.empty()||vin.empty()||pre.size()!=vin.size())
+    if (pre.empty() || vin.empty() || pre.size() != vin.size())
         return nullptr;
-    auto head=new TreeNode(pre[0]);
-    auto it=find(vin.begin(),vin.end(),pre[0]);
-    int size1=it-vin.begin();
-    vector<int> vin1(vin.begin(),it);
-    vector<int> pre1(pre.begin()+1,pre.begin()+size1+1);
-    head->left=reConstructBinaryTree(pre1,vin1);
-    head->right=reConstructBinaryTree(vector<int>(pre.begin()+size1+1,pre.end()),vector<int>(it+1,vin.end()));
+    auto head = new TreeNode(pre[0]);
+    auto it = find(vin.begin(), vin.end(), pre[0]);
+    int size1 = it - vin.begin();
+    vector<int> vin1(vin.begin(), it);
+    vector<int> pre1(pre.begin() + 1, pre.begin() + size1 + 1);
+    head->left = reConstructBinaryTree(pre1, vin1);
+    head->right = reConstructBinaryTree(vector<int>(pre.begin() + size1 + 1, pre.end()), vector<int>(it + 1, vin.end()));
     return head;
+}
+
+//求根到叶子节点数字之和
+//使用DFS递归求解
+int DFS(TreeNode* root,int sum){
+    if(!root)
+    return 0;
+    if(!root->left&&!root->right)
+    return 10*sum+root->val;
+    else
+    return DFS(root->left,10*sum+root->val)+DFS(root->right,10*sum+root->val);
+}
+
+int sumNumbers(TreeNode *root)
+{
+    return DFS(root,0);
+}
+
+//二叉树的最大深度
+//使用递归
+int Dfs_height(TreeNode* root){
+    if(!root)
+    return 0;
+    return max(Dfs_height(root->left),Dfs_height(root->right))+1;
+}
+
+int maxDepth(TreeNode* root) {
+    return  Dfs_height(root);     
+}
+
+//二叉树展开为链表
+//首先进行前序遍历将节点放入一个数组中，然后令前一个节点的右节点指向下一个节点
+vector<TreeNode*> results1;
+void preorder_traversal(TreeNode* root){
+    results1.push_back(root);
+    if(root->left)
+    preorder_traversal(root->left);
+    if(root->right)
+    preorder_traversal(root->right);
+}
+
+void flatten(TreeNode* root) {
+    preorder_traversal(root);
+    for(int i=0;i<results1.size()-1;i++){
+        auto xx=results1[i];
+        xx->left=nullptr;xx->right=results1[i+1];
+    }
+    results1[results1.size()-1]->right=nullptr;
 }
 
 int main()
